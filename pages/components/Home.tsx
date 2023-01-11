49f2529bb058797dd12cd4bdff680e2094aa5d39
Jakub Kingery <jmuhle.gc@gmail.com>
2023-01-11 14:17:22 -0700

    Added button group for voting and displayed vote state

--- pages/components/Home.tsx
+++ pages/components/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import {
   Button,
+  ButtonGroup,
   Container,
   Grid,
   Table,
@@ -34,9 +35,25 @@ const Home = () => {
   const [votes, setVotes] = useState<InitialState>({});
   const { isLoading, data } = useFetchData();
 
+  const handleUpvote = (id: string ) => {
+    if (votes[id]) {
+      setVotes((prevState) => ({ ...prevState, [id]: prevState[id] + 1}));
+    } else {
+      setVotes((prevState) => ({ ...prevState, [id]: 1}));
+    }
+  };
+
+  const handleDownvote = (id: string ) => {
+    if (votes[id]) {
+      setVotes((prevState) => ({ ...prevState, [id]: prevState[id] - 1}));
+    } else {
+      setVotes((prevState) => ({ ...prevState, [id]: -1}));
+    }
+  };
+
   return (
     <Container>
-      <h1>Jakub's Cat Facts</h1>
+      <h1>Jakub's Cat Facts UI</h1>
       <Grid>
         {isLoading && <h3>Loading...</h3>}
         {!isLoading && (
@@ -45,9 +62,8 @@ const Home = () => {
               <TableHead>
                 <TableRow>
                   <TableCell>Fact</TableCell>
-                  <TableCell>Verified</TableCell>
-                  <TableCell>Vote Count</TableCell>
-                  <TableCell align="right">
+                  <TableCell align="right">Vote Count</TableCell>
+                  <TableCell align="center">
                     Place Vote
                   </TableCell>
                 </TableRow>
@@ -56,10 +72,16 @@ const Home = () => {
                 {data?.map((row: Row) => (
                   <TableRow key={row._id}>
                     <TableCell sx={{ maxWidth: '50ch'}}>{row.text}</TableCell>
-                    <TableCell>{row.status?.verified ? 'True' : 'False'}</TableCell>
-                    <TableCell>{votes[row._id] || '0'}</TableCell>
-                    <TableCell>
-                      <Button onClick={() => console.log('vote')}>Vote!</Button>
+                    <TableCell align="right">{votes[row._id] || '0'}</TableCell>
+                    <TableCell align="right">
+                      <ButtonGroup variant="outlined">
+                        <Button onClick={() => handleDownvote(row._id)}>
+                          Downvote
+                        </Button>
+                        <Button onClick={() => handleUpvote(row._id)}>
+                          Upvote
+                        </Button>
+                      </ButtonGroup>
                     </TableCell>
                   </TableRow>
                 ))}
