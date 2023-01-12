4d9e6bf80ba908a53edaa9f07dd8e82d8efdea3f
Jakub Kingery <jmuhle.gc@gmail.com>
2023-01-11 19:38:08 -0700

    display leaderboard

--- pages/components/Home.tsx
+++ pages/components/Home.tsx
@@ -12,6 +12,7 @@ import {
   TableRow,
 } from '@mui/material';
 import { useQuery } from 'react-query';
+import { join } from 'path';
 
 const useFetchData = () => useQuery(
   'tableData1',
@@ -21,34 +22,87 @@ const useFetchData = () => useQuery(
   },
 );
 
-interface Row {
+interface FactType {
+  dataIndex: number,
+  id: string,
+  verified: boolean,
+  text: string,
+  votes: number,
+};
+
+interface RawData {
   _id: string,
   status: { verified: boolean },
   text: string,
 };
 
-interface InitialState {
-  [key:string]: number,
+interface StateType {
+  [key:string]: FactType,
+};
+
+const getHighestVotedItems = (data: StateType): string => {
+  const itemsArray = Object.values(data).reduce((acc: string[], fact: FactType, index: number): string[] => {
+    if (index === 0) {
+      acc.push(fact.id);
+      return acc;
+    } else if (data[acc[0]].votes < fact.votes) {
+      return [fact.id];
+    } else if (data[acc[0]].votes === fact.votes) {
+      acc.push(fact.id);
+      return acc;
+    }
+    return acc;
+  }, []);
+  return itemsArray.map(id => data[id].dataIndex + 1).join(', ');
+};
+
+const getLowestVotedItems = (data: StateType): string => {
+  const itemsArray = Object.values(data).reduce((acc: string[], fact: FactType, index: number): string[] => {
+    if (index === 0) {
+      acc.push(fact.id);
+      return acc;
+    } else if (data[acc[0]].votes > fact.votes) {
+      return [fact.id];
+    } else if (data[acc[0]].votes === fact.votes) {
+      acc.push(fact.id);
+      return acc;
+    }
+    return acc;
+  }, []);
+  return itemsArray.map(id => data[id].dataIndex + 1).join(', ');
 };
 
 const Home = () => {
-  const [votes, setVotes] = useState<InitialState>({});
+  const [renderData, setRenderData] = useState<StateType>({});
   const { isLoading, data } = useFetchData();
 
-  const handleUpvote = (id: string ) => {
-    if (votes[id]) {
-      setVotes((prevState) => ({ ...prevState, [id]: prevState[id] + 1}));
-    } else {
-      setVotes((prevState) => ({ ...prevState, [id]: 1}));
+  useEffect(() => {
+    if (!isLoading) {
+      setRenderData(data.reduce((acc: StateType, fact: RawData, index: number) => ({
+        ...acc,
+        [fact._id]: {
+          dataIndex: index,
+          id: fact._id,
+          verified: fact.status.verified,
+          text: fact.text,
+          votes: 0,
+        }
+      }), {}));
     }
+  }, [isLoading, data]);
+
+  const handleUpvote = (id: string ) => {
+    setRenderData((prevState) => ({
+      ...prevState,
+      [id]: { ...prevState[id], votes: prevState[id].votes + 1 },
+    }));
   };
 
   const handleDownvote = (id: string ) => {
-    if (votes[id]) {
-      setVotes((prevState) => ({ ...prevState, [id]: prevState[id] - 1}));
-    } else {
-      setVotes((prevState) => ({ ...prevState, [id]: -1}));
-    }
+    setRenderData((prevState) => ({
+      ...prevState,
+      [id]: { ...prevState[id], votes: prevState[id].votes - 1 },
+    }));
   };
 
   return (
@@ -57,37 +111,52 @@ const Home = () => {
       <Grid>
         {isLoading && <h3>Loading...</h3>}
         {!isLoading && (
-          <TableContainer>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Fact</TableCell>
-                  <TableCell align="right">Vote Count</TableCell>
-                  <TableCell align="center">
-                    Place Vote
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data?.map((row: Row) => (
-                  <TableRow key={row._id}>
-                    <TableCell sx={{ maxWidth: '50ch'}}>{row.text}</TableCell>
-                    <TableCell align="right">{votes[row._id] || '0'}</TableCell>
-                    <TableCell align="right">
-                      <ButtonGroup variant="outlined">
-                        <Button onClick={() => handleDownvote(row._id)}>
-                          Downvote
-                        </Button>
-                        <Button onClick={() => handleUpvote(row._id)}>
-                          Upvote
-                        </Button>
-                      </ButtonGroup>
+          <>
+            <div>
+              <h3>Leaderboard</h3>
+              <Grid>
+                <Grid item xs={2}>Highest votes</Grid>
+                <Grid item xs={10}>{getHighestVotedItems(renderData)}</Grid>
+              </Grid>
+              <Grid>
+                <Grid item xs={2}>Lowest votes</Grid>
+                <Grid item xs={10}>{getLowestVotedItems(renderData)}</Grid>
+              </Grid>
+            </div>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell />
+                    <TableCell>Fact</TableCell>
+                    <TableCell align="right">Vote Count</TableCell>
+                    <TableCell align="center">
+                      Place Vote
                     </TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                </TableHead>
+                <TableBody>
+                  {Object.values(renderData)?.map((row: FactType, index: number) => (
+                    <TableRow key={row.id}>
+                      <TableCell>{index + 1}</TableCell>
+                      <TableCell sx={{ maxWidth: '50ch'}}>{row.text}</TableCell>
+                      <TableCell align="right">{row.votes}</TableCell>
+                      <TableCell align="right">
+                        <ButtonGroup variant="outlined">
+                          <Button onClick={() => handleDownvote(row.id)}>
+                            Downvote
+                          </Button>
+                          <Button onClick={() => handleUpvote(row.id)}>
+                            Upvote
+                          </Button>
+                        </ButtonGroup>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </>
         )}
       </Grid>
     </Container>
